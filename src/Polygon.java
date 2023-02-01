bc3807a30d981770713f92ed137f20a623cd95fa
delcarlswlhs <delcarls@wlhs.wlwv.k12.or.us>
2023-02-01 09:54:34 -0800

    edited

--- src/Polygon.java
+++ src/Polygon.java
@@ -7,10 +7,14 @@ public class Polygon {
 
     //2 constructors
     public Polygon(){
-
+    numSides = 0;
+    sideLength = 0.0;
+    shapeType = "";
     }
-    public Polygon(int sides, double sLength, String typeShape){
-
+    public Polygon(int sides, double sLength, String sType){
+    numSides = sides;
+    sideLength = sLength;
+    shapeType = sType;
     }
 
     //accessors as needed
---
+++ src/PolygonTester.java
@@ -0,0 +1,5 @@
+public class PolygonTester {
+    public static void main(String[] args){
+
+    }
+}
