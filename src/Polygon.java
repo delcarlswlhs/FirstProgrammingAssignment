64c1c932b1fc75ef48503d8a0eb10040329f3e44
delcarlswlhs <delcarls@wlhs.wlwv.k12.or.us>
2023-02-01 09:43:59 -0800

    edited

---
+++ src/Polygon.java
@@ -0,0 +1,19 @@
+public class Polygon {
+
+    //instance variables
+    private int numSides;
+    private double sideLength;
+    private String shapeType;
+
+    //2 constructors
+    public Polygon(){
+
+    }
+    public Polygon(int sides, double sLength, String typeShape){
+
+    }
+
+    //accessors as needed
+
+    //
+}
